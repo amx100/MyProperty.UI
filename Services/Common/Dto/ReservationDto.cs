24024b5ac03ab1e3b1739ac8b92f7a16d64e13d5
Ahmed <amx281@gmail.com>
2025-04-01 05:06:52 +0200

    new datas included

--- Services/Common/Dto/ReservationDto.cs
+++ Services/Common/Dto/ReservationDto.cs
@@ -2,25 +2,25 @@
 
 public class ReservationDto
 {
-	public int Id { get; set; }
-	public DateTime StartDate { get; set; }
-	public DateTime EndDate { get; set; }
-	public int PropertyId { get; set; }
-	public string AccountId { get; set; } = string.Empty;
-	public string Status { get; set; } // "Pending", "Confirmed", "Declined"
+    public int Id { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public int PropertyId { get; set; }
+    public string PropertyTitle { get; set; } = string.Empty;
+    public string AccountId { get; set; } = string.Empty;
+    public string Status { get; set; } // "Pending", "Confirmed", "Declined"
 }
 
 public class ReservationCreateDto
 {
-	public int PropertyId { get; set; }
-	public string AccountId { get; set; } = string.Empty;
+    public int PropertyId { get; set; }
+    public string AccountId { get; set; } = string.Empty;
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
-
 }
 
 public class ReservationUpdateDto
 {
-    public int Id { get; set; }
+    public int Id { get; set; } // added new line for ui
     public string Status { get; set; } // "Pending", "Confirmed", "Declined"
 }
